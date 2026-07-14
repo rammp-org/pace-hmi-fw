719c1cf05802698176614035d0fa83e5ee16709d
Alaining <ee.master.alain@gmail.com>
2026-07-14 15:16:07 -0700

    Click sound now only plays once per click

--- main/main.cpp
+++ main/main.cpp
@@ -26,25 +26,9 @@
 
 using namespace std::chrono_literals;
 
-static constexpr size_t MAX_CIRCLES = 100;
-struct Circle {
-  int x{0};
-  int y{0};
-  int radius{0};
-  bool visible{false};
-};
-static std::array<Circle, MAX_CIRCLES> circles;
-static size_t next_circle_index = 0;
-static size_t visible_circle_count = 0;
 static std::vector<uint8_t> audio_bytes;
-static lv_obj_t *circle_layer = nullptr;
 
 static std::recursive_mutex lvgl_mutex;
-static bool initialize_circle_layer(int width, int height);
-static void draw_circle_layer(lv_event_t *event);
-static void invalidate_circle_area(const Circle &circle);
-static void draw_circle(int x0, int y0, int radius);
-static void clear_circles();
 
 static bool load_audio(size_t &out_size, size_t &out_sample_rate);
 static void play_click(espp::M5StackTab5 &tab5);
@@ -207,17 +191,15 @@ extern "C" void app_main(void) {
     if (touchpad_data != previous_touchpad_data) {
       logger.debug("Touch: {}", touchpad_data);
       previous_touchpad_data = touchpad_data;
-      // if the button is pressed, clear the circles
-      if (touchpad_data.btn_state) {
-        std::lock_guard<std::recursive_mutex> lock(lvgl_mutex);
-        clear_circles();
-      }
-      // if there is a touch point, draw a circle and play a click sound
-      if (touchpad_data.num_touch_points > 0) {
+
+      // play a click sound only on the press transition (release + re-touch
+      // required before it plays again)
+      static bool was_pressed = false;
+      bool is_pressed = touchpad_data.num_touch_points > 0;
+      if (is_pressed && !was_pressed) {
         play_click(tab5);
-        std::lock_guard<std::recursive_mutex> lock(lvgl_mutex);
-        draw_circle(touchpad_data.x, touchpad_data.y, 10);
       }
+      was_pressed = is_pressed;
     }
   };
 
@@ -358,10 +340,6 @@ extern "C" void app_main(void) {
   lv_obj_t *bg = lv_obj_create(lv_screen_active());
   lv_obj_set_size(bg, tab5.display_width(), tab5.display_height());
   lv_obj_set_style_bg_color(bg, lv_color_make(255, 255, 255), 0);
-  if (!initialize_circle_layer(tab5.display_width(), tab5.display_height())) {
-    logger.error("Failed to initialize circle layer!");
-    return;
-  }
 
   // add text in the center of the screen
   lv_obj_t *label = lv_label_create(lv_screen_active());
@@ -400,19 +378,12 @@ extern "C" void app_main(void) {
 
   static auto rotate_display = [&]() {
     std::lock_guard<std::recursive_mutex> lock(lvgl_mutex);
-    clear_circles();
     static auto rotation = LV_DISPLAY_ROTATION_0;
     rotation = static_cast<lv_display_rotation_t>((static_cast<int>(rotation) + 1) % 4);
     lv_display_t *disp = lv_display_get_default();
     lv_disp_set_rotation(disp, rotation);
     // update the size of the screen
     lv_obj_set_size(bg, tab5.rotated_display_width(), tab5.rotated_display_height());
-    if (circle_layer) {
-      lv_obj_set_size(circle_layer, tab5.rotated_display_width(), tab5.rotated_display_height());
-      lv_obj_align(circle_layer, LV_ALIGN_CENTER, 0, 0);
-      lv_obj_move_foreground(circle_layer);
-      lv_obj_invalidate(circle_layer);
-    }
   };
 
   // add a button in the top left which (when pressed) will rotate the display
@@ -431,7 +402,6 @@ extern "C" void app_main(void) {
   // rotated and drawing with your finger)
   lv_obj_set_scrollbar_mode(lv_screen_active(), LV_SCROLLBAR_MODE_OFF);
   lv_obj_clear_flag(lv_screen_active(), LV_OBJ_FLAG_SCROLLABLE);
-  lv_obj_move_foreground(circle_layer);
 
   // Load the SquareLine Studio UI. This creates ui_Screen1 and makes it the
   // active screen; the demo widgets above stay on the (now hidden) default
@@ -696,104 +666,6 @@ extern "C" void app_main(void) {
     std::this_thread::sleep_for(1s);
   }
   //! [m5stack tab5 example]
-}
-
-static bool initialize_circle_layer(int width, int height) {
-  if (circle_layer) {
-    return true;
-  }
-  circle_layer = lv_obj_create(lv_screen_active());
-  if (!circle_layer) {
-    return false;
-  }
-  lv_obj_remove_style_all(circle_layer);
-  lv_obj_set_size(circle_layer, width, height);
-  lv_obj_align(circle_layer, LV_ALIGN_CENTER, 0, 0);
-  lv_obj_clear_flag(circle_layer, LV_OBJ_FLAG_CLICKABLE);
-  lv_obj_clear_flag(circle_layer, LV_OBJ_FLAG_SCROLLABLE);
-  lv_obj_set_style_bg_opa(circle_layer, LV_OPA_TRANSP, 0);
-  lv_obj_set_style_border_width(circle_layer, 0, 0);
-  lv_obj_set_style_outline_width(circle_layer, 0, 0);
-  lv_obj_set_style_shadow_width(circle_layer, 0, 0);
-  lv_obj_add_event_cb(circle_layer, draw_circle_layer, LV_EVENT_DRAW_MAIN, nullptr);
-  return true;
-}
-
-static void draw_circle_layer(lv_event_t *event) {
-  if (visible_circle_count == 0) {
-    return;
-  }
-  auto *obj = static_cast<lv_obj_t *>(lv_event_get_current_target(event));
-  auto *layer = lv_event_get_layer(event);
-  lv_area_t obj_coords;
-  lv_obj_get_coords(obj, &obj_coords);
-
-  lv_draw_rect_dsc_t rect_dsc;
-  lv_draw_rect_dsc_init(&rect_dsc);
-  rect_dsc.base.layer = layer;
-  rect_dsc.radius = LV_RADIUS_CIRCLE;
-  rect_dsc.bg_opa = LV_OPA_70;
-  rect_dsc.bg_color = lv_color_make(0, 255, 255);
-  rect_dsc.border_width = 0;
-  rect_dsc.outline_width = 0;
-  rect_dsc.shadow_width = 0;
-
-  for (const auto &circle : circles) {
-    if (!circle.visible) {
-      continue;
-    }
-    lv_area_t coords = {
-        .x1 = static_cast<lv_coord_t>(obj_coords.x1 + circle.x - circle.radius),
-        .y1 = static_cast<lv_coord_t>(obj_coords.y1 + circle.y - circle.radius),
-        .x2 = static_cast<lv_coord_t>(obj_coords.x1 + circle.x + circle.radius - 1),
-        .y2 = static_cast<lv_coord_t>(obj_coords.y1 + circle.y + circle.radius - 1),
-    };
-    lv_draw_rect(layer, &rect_dsc, &coords);
-  }
-}
-
-static void invalidate_circle_area(const Circle &circle) {
-  if (!circle_layer || circle.radius <= 0) {
-    return;
-  }
-
-  lv_area_t obj_coords;
-  lv_obj_get_coords(circle_layer, &obj_coords);
-  lv_area_t coords = {
-      .x1 = static_cast<lv_coord_t>(obj_coords.x1 + circle.x - circle.radius),
-      .y1 = static_cast<lv_coord_t>(obj_coords.y1 + circle.y - circle.radius),
-      .x2 = static_cast<lv_coord_t>(obj_coords.x1 + circle.x + circle.radius - 1),
-      .y2 = static_cast<lv_coord_t>(obj_coords.y1 + circle.y + circle.radius - 1),
-  };
-  lv_obj_invalidate_area(circle_layer, &coords);
-}
-
-static void draw_circle(int x0, int y0, int radius) {
-  if (!circle_layer) {
-    return;
-  }
-  lv_obj_move_foreground(circle_layer);
-  Circle previous_circle = circles[next_circle_index];
-  circles[next_circle_index] = {.x = x0, .y = y0, .radius = radius, .visible = true};
-  next_circle_index = (next_circle_index + 1) % circles.size();
-  if (visible_circle_count < circles.size()) {
-    visible_circle_count++;
-  }
-  if (previous_circle.visible) {
-    invalidate_circle_area(previous_circle);
-  }
-  invalidate_circle_area(circles[(next_circle_index + circles.size() - 1) % circles.size()]);
-}
-
-static void clear_circles() {
-  for (auto &circle : circles) {
-    if (circle.visible) {
-      invalidate_circle_area(circle);
-    }
-    circle.visible = false;
-  }
-  next_circle_index = 0;
-  visible_circle_count = 0;
 }
 
 static bool load_audio(size_t &out_size, size_t &out_sample_rate) {
